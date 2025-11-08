c26724d6f78bf12ab29f14d25653142387f60bc7
PratyutCS <pratyut987@gmail.com>
2025-11-09 04:30:47 +0530

    Feat: Remove largest background images from carousel
    Removed import statements and references for the 5 largest background images from src/components/BackgroundCarousel.jsx. This aims to reduce the initial load size and improve carousel loading performance by reducing the number of heavy assets.
    
    NOTE: This is a workaround to address loading issues due to large file sizes. Optimal solution still requires manual image optimization (resizing and compressing) of all remaining images.

--- frontend/src/components/BackgroundCarousel.jsx
+++ frontend/src/components/BackgroundCarousel.jsx
@@ -2,15 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 // Import your background images
 import img1 from '../../assets/background/bg_image_1.jpg';
-import img2 from '../../assets/background/bg_image_2.jpg';
 import img3 from '../../assets/background/bg_image_3.jpg';
-import img4 from '../../assets/background/bg_image_4.jpg';
 import img5 from '../../assets/background/bg_image_5.jpg';
 import img6 from '../../assets/background/bg_image_6.jpg';
 import img7 from '../../assets/background/bg_image_7.jpg';
-import img8 from '../../assets/background/bg_image_8.jpg';
-import img9 from '../../assets/background/bg_image_9.jpg';
-import img10 from '../../assets/background/bg_image_10.jpg';
 import img11 from '../../assets/background/bg_image_11.jpeg';
 import img12 from '../../assets/background/bg_image_12.jpeg';
 import img13 from '../../assets/background/bg_image_13.jpeg';
@@ -18,15 +13,10 @@ import img14 from '../../assets/background/bg_image_14.jpg';
 
 const images = [
   `url(${img1})`,
-  `url(${img2})`,
   `url(${img3})`,
-  `url(${img4})`,
   `url(${img5})`,
   `url(${img6})`,
   `url(${img7})`,
-  `url(${img8})`,
-  `url(${img9})`,
-  `url(${img10})`,
   `url(${img11})`,
   `url(${img12})`,
   `url(${img13})`,

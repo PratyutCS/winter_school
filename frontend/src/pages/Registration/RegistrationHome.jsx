f665434714e99bda08048cda9b09ef73845d197f
Prathamesh Ram <rprathamesh@iitbhilai.ac.in>
2025-11-22 16:56:43 +0530

    Modified registration details

--- frontend/src/pages/Registration/RegistrationHome.jsx
+++ frontend/src/pages/Registration/RegistrationHome.jsx
@@ -45,7 +45,7 @@ export default function RegistrationHome() {
                   </li>
                   <li className="flex items-start">
                     <svg className="w-6 h-6 text-[#7c3aed] mr-3 mt-1 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H4a3 3 0 00-3 3v8a3 3 0 003 3z" /></svg>
-                    <div><strong>Travel Reimbursement:</strong> Students are eligible for travel expense reimbursement up to 3rd AC train fare or equivalent, as per institute norms.</div>
+                    <div><strong>Travel Reimbursement:</strong> Students are eligible for travel expense reimbursement of either 3rd AC train fare or bus fare upto {"\u20B9"}1000 (each for arrival and departure)</div>
                   </li>
                 </ul>
               </div>

1a69f8916c8d5a0d35e9a5e46190b02178d97228
PratyutCS <pratyut987@gmail.com>
2025-12-10 10:25:15 +0530

    updated speaker details

--- exec.sh
+++ exec.sh
@@ -11,22 +11,14 @@ echo "Pushing to remote..."
 git push
 
 echo "Running local script..."
-
-cd frontend
-npm i
-printf "\nSatisfied preview, building now\n"
-npm run build
-cd dist
-zip -r website.zip ./
-printf "\nscp frontend/dist/website.zip cws@events.iitbhilai.ac.in:~/\n\n"
-
+./script_0.sh
 
 echo "Zipping frontend/dist (if not already zipped)..."
 # Uncomment the next line if you need to zip it here:
 # zip -r frontend/dist/website.zip frontend/dist/*
 
 echo "Copying build to server..."
-scp ./frontend/dist/website.zip cws@events.iitbhilai.ac.in:~/
+scp frontend/dist/website.zip cws@events.iitbhilai.ac.in:~/
 
 echo "SSH into server..."
 ssh cws@events.iitbhilai.ac.in
---
+++ script_0.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd frontend
+npm i
+# npm run dev
+printf "\nSatisfied preview, building now\n"
+npm run build
+cd dist
+zip -r website.zip ./
+printf "\nscp frontend/dist/website.zip cws@events.iitbhilai.ac.in:~/\n\n"
